f3041859340965ef3f52977c246d29795f6dc8f1
nnnp-arol <arol.alonso@nonerds.com>
2023-03-27 01:20:31 -0300

    comm2

--- src/db.ts
+++ src/db.ts
@@ -6,7 +6,9 @@ import mongoose from "mongoose";
 export const dbConnect = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const db = await mongoose.connect(process.env.MONGODB_URI);
+    const db = await mongoose.connect(
+      "mongodb+srv://arolalonso:asdqwe123DEV@cluster0.kabyj3b.mongodb.net/lotesuer?retryWrites=true&w=majority"
+    );
     console.log("db is connected to ", db.connection.db.databaseName);
   } catch (error) {
     if (error instanceof Error) {
